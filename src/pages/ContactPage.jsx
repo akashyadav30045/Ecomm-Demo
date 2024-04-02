0f188ca8969d7daeb024f24458f98fa3f1737536
Akash Yadav <akashyadav30045@gmail.com>
2024-04-02 17:48:37 +0530

    Initial

---
+++ src/pages/ContactPage.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import {Navbar} from "../components";
+const ContactPage = () => {
+  return (
+    <>
+        <Navbar />
+        <>
+        <div className="container my-3 py-3">
+            <h1 className="text-center">Contact Us</h1>
+            <hr />
+            
+        </div>
+    </>
+    </>
+  )
+}
+
+export default ContactPage
